ffc4163f2b55cd4f3441fb7fb18867bc0e1b4fb1
Huynh Hoang <hotodghoang@gmail.com>
2025-07-01 13:08:23 +0700

    Create map page

--- flutter_check/lib/main.dart
+++ flutter_check/lib/main.dart
@@ -1,9 +1,19 @@
-import 'package:flutter_check/chat_box_map.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_check/view/chat_box_map.dart';
+import 'package:flutter_check/view/map.dart';
+import 'package:device_preview/device_preview.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+import 'package:device_preview/device_preview.dart';
+
+void main() => runApp(
+  DevicePreview(
+    enabled: !kReleaseMode,
+    builder: (context) => MyApp(), // Wrap your app
+  ),
+);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
